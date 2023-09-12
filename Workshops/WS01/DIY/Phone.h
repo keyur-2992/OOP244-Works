7edc39a82ddc232e47a0eb014c82aace87c10877
Keyur Patel <keyurpatel.ca29@gmail.com>
2023-09-12 15:57:34 -0400

    Completed DIY (Phone.cpp,Phone.h)

--- Workshops/WS01/DIY/Phone.h
+++ Workshops/WS01/DIY/Phone.h
@@ -1,3 +1,17 @@
+/*
+*****************************************************************************
+						  Workshop 1 DIY
+Full Name  : keyurkumar shankarlal patel
+Student ID#: 170852214
+Email      : kpatel453@myseneca.ca
+Section    : ZAA
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
 #ifndef SDDS_PHONE_H 
 #define SDDS_PHONE_H
 
